fcba89e15b97c8b8cc979bf501258dc895447bca
Joseph Lin <josephlin55555@gmail.com>
2020-03-08 19:34:49 -0400

    [added] sprites

--- src/Widget/index.jsx
+++ src/Widget/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './sprite.css';
 
 export default class Widget extends Component {
   state = {
@@ -11,13 +12,23 @@ export default class Widget extends Component {
       .get('http://s3.amazonaws.com/com.veea.medals/medals.json')
       .then(({data}) => {
         this.setState({ data });
-      });
+      })
+      .catch(() => console.log("ERROR"))
   }
 
   render() {
     console.log(this.state.data)
     return (
-      <div>Widget</div>
+      <div>
+        <div className="flag first" />
+        <br />
+        <div className="flag second" />
+        <br />
+        <div className="flag third" />
+        <br />
+        <div className="flag fourth" />
+
+      </div>
     );
   }
 }
