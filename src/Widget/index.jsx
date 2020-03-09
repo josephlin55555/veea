c2300f5cbb17f1a9a457453ea640467c8bedd955
Joseph Lin <josephlin55555@gmail.com>
2020-03-08 20:15:12 -0400

    [init] general application

--- src/Widget/index.jsx
+++ src/Widget/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './sprite.css';
+import './index.css';
 
 export default class Widget extends Component {
   state = {
@@ -11,7 +11,13 @@ export default class Widget extends Component {
     axios
       .get('http://s3.amazonaws.com/com.veea.medals/medals.json')
       .then(({data}) => {
-        this.setState({ data });
+        this.setState({
+          data: data
+            .map(datum => {
+              datum.total = datum.gold + datum.silver + datum.bronze;
+              return datum;
+            })
+        });
       })
       .catch(() => console.log("ERROR"))
   }
@@ -19,16 +25,35 @@ export default class Widget extends Component {
   render() {
     console.log(this.state.data)
     return (
-      <div>
-        <div className="flag first" />
-        <br />
-        <div className="flag second" />
-        <br />
-        <div className="flag third" />
-        <br />
-        <div className="flag fourth" />
-
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th><div className="circle gold" /></th>
+            <th><div className="circle silver" /></th>
+            <th><div className="circle bronze" /></th>
+            <th>TOTAL</th>
+          </tr>
+          {
+            this.state.data
+              .map((datum, index) => {
+                return (
+                  <tr>
+                    <td>{index}</td>
+                    <td><div className={`flag ${datum.code.toLowerCase()}`} /></td>
+                    <td>{datum.code}</td>
+                    <td>{datum.gold}</td>
+                    <td>{datum.silver}</td>
+                    <td>{datum.bronze}</td>
+                    <td>{datum.total}</td>
+                  </tr>
+                );
+              })
+          }
+        </tbody>
+      </table>
     );
   }
 }
